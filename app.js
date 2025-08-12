a8f0b5a4442cdd98bd2fad56b0d997f764ae621c
Tajwar Hossain <107741603+tajwarsami@users.noreply.github.com>
2025-08-13 00:30:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const auth = require("./routes/auth");
 const list = require("./routes/list");
 app.use(express.json());
 app.use(cors());
-app.get("/" , (req, res) => {
-  res.send("Hello");
-});
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
 
